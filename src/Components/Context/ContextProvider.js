93d31494df00086b702fc1f965b03f63b1043adf
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-23 21:02:02 +0600

    first commit

---
+++ src/Components/Context/ContextProvider.js
@@ -0,0 +1,73 @@
+// import React, { createContext, useEffect, useState } from "react";
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+//   onAuthStateChanged,
+//   signOut,
+//   sendPasswordResetEmail,
+// } from "firebase/auth";
+// import app from "../../firebase/firebase.config";
+
+// export const AuthContext = createContext();
+// const auth = getAuth(app);
+// const googleProvider = new GoogleAuthProvider();
+
+// const AuthProvider = ({ children }) => {
+//   const [loader, setLoader] = useState(true);
+//   const [user, setUser] = useState({});
+
+//   //create user
+//   const userRegister = (email, password) => {
+//     setLoader(true);
+//     return createUserWithEmailAndPassword(auth, email, password);
+//   };
+
+//   //login user
+//   const userLogin = (email, password) => {
+//     setLoader(true);
+//     return signInWithEmailAndPassword(auth, email, password);
+//   };
+
+//   //sign in with google
+//   const loginWithGoogle = () => {
+//     setLoader(true);
+//     return signInWithPopup(auth, googleProvider);
+//   };
+
+//   //get current user
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//       setUser(currentUser);
+//       setLoader(false);
+//     });
+//     return () => unsubscribe();
+//   }, []);
+
+//   //reset email
+//   const userPasswordReset = (email) => {
+//     setLoader(true);
+//     return sendPasswordResetEmail(auth, email);
+//   };
+//   //user sign out
+//   const userSignOut = () => {
+//     setLoader(true);
+//     return signOut(auth);
+//   };
+//   const authInfo = {
+//     userLogin,
+//     userRegister,
+//     loginWithGoogle,
+//     userSignOut,
+//     user,
+//     loader,
+//     userPasswordReset,
+//   };
+//   return (
+//     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+//   );
+// };
+
+// export default AuthProvider;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const navLinks = (
+    <React.Fragment>
+      <Link to="/home" className=" rounded-xl">
+        Home
+      </Link>
+      <Link className="rounded-lg">About</Link>
+      <Link to="/appointmentHome" className="rounded-lg">
+        Appointment
+      </Link>
+      <Link className="rounded-lg">Reviews</Link>
+      <Link to="/dashBoard" className="rounded-lg">
+        Dash Board
+      </Link>
+      {/* {user?.email ? (
+        <>
+          <Link onClick={userSignOut} className="rounded-lg">
+            LOG OUT
+          </Link>
+          <small className="font-bold">{user?.email}</small>
+        </>
+      ) : (
+        <Link to="/login" className="rounded-lg">
+          LOG IN
+        </Link>
+      )} */}
+      {/* <input type="checkbox" className="toggle toggle-md rounded-full" /> */}
+    </React.Fragment>
+  );
+  return (
+    <div className="navbar lg:flex lg:justify-between justify-between items-center bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact  dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>{navLinks}</li>
+          </ul>
+        </div>
+        <Link className="btn btn-ghost normal-case text-xl">Doctor Portal</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0 ">
+          <li>{navLinks}</li>
+        </ul>
+      </div>
+      <label
+        htmlFor="my-drawer"
+        tabIndex={1}
+        className="btn btn-ghost lg:hidden"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+      </label>
+    </div>
+  );
+};
+
+export default Header;
