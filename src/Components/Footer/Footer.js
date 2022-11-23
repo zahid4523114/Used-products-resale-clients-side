2a95a43a002cec522e45c44b345f00b8647cfc50
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-23 23:30:57 +0600

    banner-added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <p>
             Camsec Industries Ltd.
             <br />
-            Providing reliable tech since 1992
+            Providing reliable tech since.
           </p>
         </div>
         <div>
